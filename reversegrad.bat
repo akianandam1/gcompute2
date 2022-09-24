b964e8ed21b3ddfec3d5db1943a3b745d021ac16
akianandam1 <62275636+akianandam1@users.noreply.github.com>
2022-09-24 10:10:39 -0700

    Create reversegrad.bat

---
+++ reversegrad.bat
@@ -0,0 +1,9 @@
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+powershell.exe -Command "C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+pause
