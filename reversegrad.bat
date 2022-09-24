d4c5d8ae3f563f2509ad76c99661ba0488c039aa
akianandam1 <62275636+akianandam1@users.noreply.github.com>
2022-09-24 10:16:52 -0700

    Update reversegrad.bat

--- reversegrad.bat
+++ reversegrad.bat
@@ -1,10 +1,9 @@
-
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
-start cmd /k "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
+start powershell.exe -Command "cd C:\Users\Lazarus\Desktop\ReverseGrad ; python ReverseGradient.py"
 pause
